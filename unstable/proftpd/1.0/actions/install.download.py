48f71d3da2eff3a651fc6ddab713c6c5bed5b63c
kristof <kristof@incubaid.com>
2013-12-09 15:59:03 +0200

    refactor

--- unstable/proftpd/1.0/actions/install.download.py
+++ unstable/proftpd/1.0/actions/install.download.py
@@ -1,4 +1,4 @@
-def main(j,jp,destination=None):
+def main(j,jp,destination=None,force=False,expand=True,nocode=False):
    
     #copying of files is done in this step
-    jp._download()
+    jp._download(force=force,destination=destination,expand=expand,nocode=nocode)
