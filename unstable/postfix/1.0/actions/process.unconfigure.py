89e441fbf408e0f95bfe8489cf13572b2d03ad78
kristof <kristof@incubaid.com>
2014-02-17 11:31:33 +0200

    jpackage wth better branch support tested & bugfixes

--- unstable/postfix/1.0/actions/process.unconfigure.py
+++
@@ -1,9 +0,0 @@
-def main(j,jp):
-   
-    #configure the application to no longer autostart
-    
-    jp.log("remove autostart $(jp.name)")
-    jp.kill()
-    j.tools.startupmanager.removeProcess(jp.name)
-
-
--- unstable/postfix/1.0/actions/uninstall.py
+++
@@ -1,3 +0,0 @@
-def main(j,jp):
-   
-    jp._uninstall()
