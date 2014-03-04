fbcb4452d16ddcdca131e8b88fc44f7a686ef98a
kristof <kristof@incubaid.com>
2014-03-04 11:11:23 +0100

    webmin

--- unstable/webmin/1.0/actions/install.post.py
+++ unstable/webmin/1.0/actions/install.post.py
@@ -1,15 +1,8 @@
 def main(j,jp):
    
-    #copying of files is not done in this step
-    # the order is:
-    # first do prepare
-    # then the system automatically copies (if not in debug) starting from the files section of the jpackage
-    # then do this tasklet (postinstall)
+    do=j.system.process.execute
+    do("apt-get install -f -y")
+    do("/etc/init.d/webmin restart")
 
-    #shortcut to some usefull install tools
-    #do=j.system.installtools
-
-    #configuration is not done in this step !!!!!
-    #preparation like system preps like ubuntu deb installs also not done here
     
     pass
--- unstable/webmin/1.0/actions/install.prepare.py
+++ unstable/webmin/1.0/actions/install.prepare.py
@@ -2,20 +2,14 @@ def main(j,jp):
    
     #prepare the platform before copying the files
 
-    # can happen by e.g. installing a debian package e.g. by
-    packages='libnet-ssleay-perl  libapt-pkg-perl libauthen-pam-perl libio-pty-perl'
-    for packagename in packages.split(" "):
-        print "install %s"%packagename
-        packagename=packagename.strip()
-        if packagename<>"":
-            j.system.platform.ubuntu.install(packagename)
-       
     #install found debs they need to be in debs dir of one or more of the platforms (all relevant platforms will be used)
     #args.qp.installUbuntuDebs()
     
     #shortcut to some usefull install tools
-    do=j.system.installtools
-    do.execute("apt-get install -f -y")
+    do=j.system.process.execute
+    do("apt-get install -f -y")
+    do("apt-get install libnet-ssleay-perl  libapt-pkg-perl libauthen-pam-perl libio-pty-perl -y")
+    do("apt-get install -f -y")
 
     #configuration is not done in this step !!!!!
     #copying files from files section of jpackages is not done in this step
