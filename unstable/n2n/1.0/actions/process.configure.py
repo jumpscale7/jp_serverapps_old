f3f78c2af71d5215f7072702cc73ec7d0e56e0d6
kristof <kristof@incubaid.com>
2014-04-14 16:56:15 +0200

    update

--- unstable/n2n/1.0/actions/process.configure.py
+++ unstable/n2n/1.0/actions/process.configure.py
@@ -15,7 +15,6 @@ def main(j,jp):
 
 
     
-
     args='-a %s -c %s -k %s -l %s:86 -s 255.255.0.0 -f'%(str(net2.ip),netname,secret,tracker)
     # print args
     cmd="%s %s"%('/usr/sbin/edge', args) #actual n2n command to run
@@ -23,8 +22,16 @@ def main(j,jp):
     cmd="%s & pid=$!; while true; do c=1; ping 10.10.100.5 -c1 >/dev/null || c=0; if [ \"$c\" == \"0\" ]; then kill $pid; exit 1; fi; sleep 5; done"%(cmd)   # die on ping failure
     # print cmd
 
+    print cmd
+
+    print "stop n2n",
     j.system.platform.ubuntu.stopService('n2n')
-    j.system.platform.ubuntu.serviceInstall('n2n', cmd)
+    print "ok"
+    print "stop install service n2n"
+    j.system.platform.ubuntu.serviceInstall('n2n', cmd,reload=False)
+    print "ok"
+    print "start n2n"
     j.system.platform.ubuntu.startService('n2n')
+    print "ok"
 
     # edge -a 10.1.2.1 -c mynetwork -k encryptme -l a.b.c.d:xyw
