2e3475e26d46c94bf3e2ab1b842aec4cbd32f93c
Jo De Boeck <deboeckj@codescalers.com>
2014-06-25 16:41:41 +0300

    Add httprobot to jpackage

--- unstable/mailrobot/1.0/actions/process.configure.py
+++ unstable/mailrobot/1.0/actions/process.configure.py
@@ -30,3 +30,28 @@ def main(j,jp):
         processfilterstr="mailrobot.py")#what to look for when doing ps ax to find the process
     
     pd.start()
+
+    pd=j.tools.startupmanager.addProcess(\
+        name='httprobot',\
+        cmd=cmd, \
+        args="mailrobot.py",\
+        env={},\
+        numprocesses=1,\
+        priority=100,\
+        shell=False,\
+        workingdir='$base/apps/httprobot',\
+        jpackage=jp,\
+        domain=jp.domain,\
+        ports=[],\
+        autostart=0,\
+        reload_signal=0,\
+        user="root",\
+        log=True,\
+        stopcmd=None,\
+        check=True,\
+        timeoutcheck=20,\
+        isJSapp=0,\
+        upstart=False,\
+        stats=True,\
+        processfilterstr="httprobot.py")#what to look for when doing ps ax to find the process
+    pd.start()
