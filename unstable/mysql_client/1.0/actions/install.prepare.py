b73c6e4a681333a9a42292f0a05639928c871f61
despiegk <devnull@localhost>
2014-07-05 14:32:29 +0200

    osticket

---
+++ unstable/mysql_client/1.0/actions/install.prepare.py
@@ -0,0 +1,18 @@
+def main(j,jp):
+   
+    #prepare the platform before copying the files
+
+    # can happen by e.g. installing a debian package e.g. by
+    j.system.platform.ubuntu.install("mysql-client")
+    j.system.platform.ubuntu.install("python-mysqldb")
+       
+    #install found debs they need to be in debs dir of one or more of the platforms (all relevant platforms will be used)
+    #args.qp.installUbuntuDebs()
+    
+    #shortcut to some usefull install tools
+    #do=j.system.installtools
+
+    #configuration is not done in this step !!!!!
+    #copying files from files section of jpackages is not done in this step
+    
+    pass
