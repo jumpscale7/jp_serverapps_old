59ce0641d2c98bb27c751682842a58cee32eebf3
kristof <kristof@incubaid.com>
2014-08-06 07:38:55 +0200

    jpackages: had to remove locks

--- unstable/influxdb/1.0/actions/process.configure.py
+++ unstable/influxdb/1.0/actions/process.configure.py
@@ -5,6 +5,6 @@ def main(j,jp):
     cfgfile = j.system.fs.joinPaths(j.dirs.cfgDir, 'influxdb', 'config.toml')
     args = "-config=%s" % cfgfile
     startstoptimeout = 60
-    processfilterstr = "influxdb"
-    j.tools.startupmanager.addProcess(name, cmd, workingdir=workingdir, args=args, domain="serverapps",jpackage=jp,ports=[],priority=1,\
+    processfilterstr = "./influxdb -config"
+    j.tools.startupmanager.addProcess(name, cmd, workingdir=workingdir, args=args, domain="serverapps",jpackage=jp,ports=[8086,8090,8083],priority=1,\
         check=True,timeoutcheck=startstoptimeout,isJSapp=0,processfilterstr=processfilterstr,stats=True,upstart=False)
