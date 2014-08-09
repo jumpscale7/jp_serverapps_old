ecd8563ce65a698b85dcb527d6049968f336d9f5
kristof <kristof@incubaid.com>
2014-08-09 08:26:31 +0200

    jpackage updates & cloudrobot & jpackages cleanup

--- unstable/robot/1.0/actions/install.download.py
+++
@@ -1,4 +0,0 @@
-def main(j,jp,destination=None,force=False,expand=True,nocode=False):
-   
-    #copying of files is done in this step
-    jp._download(force=force,destination=destination,expand=expand,nocode=nocode)
--- unstable/robot/1.0/actions/process.depcheck.py
+++
@@ -1,8 +0,0 @@
-def main(j,jp):
-   
-    #check dependencies for process to start e.g. can I reach local or remote app
-
-    #e.g.
-    #return j.system.net.tcpPortConnectionTest(ipaddr, 9200)
-
-    pass
